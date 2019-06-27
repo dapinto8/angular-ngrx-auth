6bfb31c1cf68ab6238eca15001bb0a5998f4bf26
David Pinto <dpinto@jacidi.com>
2019-06-27 08:32:47 -0400

    Repository name

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-ngrx-auth'`, () => {
+  it(`should have as title 'gleep-auth'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('angular-ngrx-auth');
+    expect(app.title).toEqual('gleep-auth');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to angular-ngrx-auth!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to gleep-auth!');
   });
 });
